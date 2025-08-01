127445929d4cc7775dc7af552b8493bcf98ab2c4
GitHub Action <action@github.com>
2025-08-01 12:23:41 +0000

    chore: release v0.5.0
    - Bump version to 0.5.0
    - Update CHANGELOG.md

--- src/s2dm/__init__.py
+++ src/s2dm/__init__.py
@@ -4,7 +4,7 @@ from rich.logging import RichHandler
 
 __author__ = """Daniel Alvarez-Coello"""
 __email__ = "8550265+jdacoello@users.noreply.github.com"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 logging.basicConfig(
     level="INFO",
