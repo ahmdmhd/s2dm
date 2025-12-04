6f7e2a1bc5ccd7064ed15a40c81fbf6b1c2a3cc4
GitHub Action <action@github.com>
2025-12-04 12:13:14 +0000

    chore: release v0.9.1
    - Bump version to 0.9.1
    - Update CHANGELOG.md

--- src/s2dm/__init__.py
+++ src/s2dm/__init__.py
@@ -2,6 +2,6 @@ from s2dm.logger import get_logger
 
 __author__ = """Daniel Alvarez-Coello"""
 __email__ = "8550265+jdacoello@users.noreply.github.com"
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 log = get_logger("s2dm")
