56ec7102301f6d9b90c33099716918946318b32e
JD Alvarez <8550265+jdacoello@users.noreply.github.com>
2026-07-23 16:56:12 +0200

    fix: BaseURL for voyager
    Signed-off-by: JD Alvarez <8550265+jdacoello@users.noreply.github.com>

--- src/s2dm/templates/docs-website/src/pages/visualizer.tsx
+++ src/s2dm/templates/docs-website/src/pages/visualizer.tsx
@@ -1,10 +1,12 @@
 import Layout from "@theme/Layout";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function VoyagerPage() {
+  const voyagerSrc = useBaseUrl("/voyager.html");
   return (
     <Layout title="Schema Visualizer" noFooter>
       <iframe
-        src="/voyager.html"
+        src={voyagerSrc}
         style={{ width: "100%", height: "calc(100vh - 60px)", border: "none" }}
         title="GraphQL Voyager"
       />
